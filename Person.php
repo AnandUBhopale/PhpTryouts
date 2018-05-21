03fca02bc9d630d9f60b7bb2dd11e1220323ce77
Anand Bhopale <anand.bhopaley@gmail.com>
2018-05-21 14:50:21 +0200

    First checkin

---
+++ Person.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dedub
+ * Date: 5/20/2018
+ * Time: 9:41 PM
+ */
+
+class Person
+{
+    public $firstname;
+
+    public function SayHello()
+    {
+        echo "Hello" .  $this->firstname;
+
+    }
+}
+
+$myPerson = new Person();
+
+$myPerson ->firstname ="Anand";
+
+echo "Person Name : ". $myPerson->firstname;
+
+echo "Say". $myPerson->SayHello();
---
+++ SendEmail.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dedub
+ * Date: 5/20/2018
+ * Time: 10:25 PM
+ */
+
+class SendEmail
+{
+    public $sender;
+    public $subject;
+    public $body;
+    public $recipients;
+
+    /**
+     * @param $sender
+     */
+    function __contrust($mySender)
+    {
+        $this->sender = $mySender;
+        $this->recipients =array();
+    }
+
+    /**
+     * @param $recipients
+     */
+    function addRecipients($recipients){
+        array_push($this->recipients, $recipients );
+    }
+
+    /**
+     * @param $subject
+     */
+    function setsubject ($subject){
+        $this ->subject = $subject;
+    }
+
+    function setBody ($body){
+        $this->body = $body;
+    }
+
+    function sendEmail (){
+
+        foreach ($this->recipients as $recipients) {
+
+            $result = mail($recipients,$this->subject,$this->body);
+            if ($result) echo "Mail successfully sent to 
+                                        {$recipients}<br/>";
+
+        }
+    }
+}
+
+$emailer = new SendEmail("anand.bhopale@gmail.com");
+
+$emailer-> setsubject("New Subject") ;
+$emailer -> setBody("Body of email, Type anything");
+$emailer -> addRecipients("anand.bhople@gmail.com; anandbhopale.u@cognizant.com");
+$emailer -> sendEmail();
+
+echo 'Email has sent successufully';
